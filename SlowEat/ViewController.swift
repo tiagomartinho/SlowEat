5d5c091e4761acfb6a901b447800b5d63f16b685
Tiago Martinho <t.martinho@icloud.com>
2018-07-01 16:11:15 +0200

    Initial Commit

---
+++ SlowEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlowEat
+//
+//  Created by Tiago Martinho on 1/7/18.
+//  Copyright © 2018 tm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
