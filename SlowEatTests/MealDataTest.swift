eb39e4c0e0117af9c871ced71d26104c70d27214
Tiago Martinho <t.martinho@icloud.com>
2018-07-02 17:05:23 +0200

    Add MealDataTest

---
+++ SlowEatTests/MealDataTest.swift
@@ -0,0 +1,19 @@
+@testable import SlowEat
+import XCTest
+
+class MealDataTest: XCTestCase {
+    func testSerialization() {
+        let meal = Meal(startTime: 123)
+        let mealData = MealData(meal: meal)
+        let data = NSKeyedArchiver.archivedData(withRootObject: mealData)
+        let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+        let decodedMeal = unarchiver.decodeObject(forKey: "root") as? MealData
+        XCTAssertEqual(decodedMeal?.meal, meal)
+    }
+
+    func testMealSerialization() {
+        let meal = Meal(startTime: 123)
+        let otherMeal = Meal(any: meal.data)
+        XCTAssertEqual(meal, otherMeal)
+    }
+}
