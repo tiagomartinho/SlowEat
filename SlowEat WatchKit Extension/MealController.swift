c8148564c2ff6b88ce5a345133e3f0fc72c03567
Tiago Martinho <t.martinho@icloud.com>
2018-07-03 08:36:51 +0200

    Remove InitialController for navigation

--- SlowEat WatchKit Extension/MealController.swift
+++ SlowEat WatchKit Extension/MealController.swift
@@ -5,4 +5,15 @@ class MealController: WKInterfaceController {
     lazy var presenter = MealPresenter(timeProvider: FoundationTimeProvider(),
                                   mealTransfer: WKMealTransfer(),
                                   router: WKRouter(controller: self))
+
+    @IBOutlet var motivationLabel: WKInterfaceLabel!
+
+    override func willActivate() {
+        super.willActivate()
+        motivationLabel.setText(MotivationalQuotes.random)
+    }
+
+    @IBAction func startMeal() {
+        presenter.track()
+    }
 }
