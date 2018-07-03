b97031930af70094bd5090d702c4681e976e5d66
Tiago Martinho <t.martinho@icloud.com>
2018-07-03 08:06:48 +0200

    Add Initial Controller

--- SlowEat WatchKit Extension/MealController.swift
+++ SlowEat WatchKit Extension/MealController.swift
@@ -5,8 +5,4 @@ class MealController: WKInterfaceController {
     lazy var presenter = MealPresenter(timeProvider: FoundationTimeProvider(),
                                   mealTransfer: WKMealTransfer(),
                                   router: WKRouter(controller: self))
-
-    @IBAction func trackMeal() {
-        presenter.track()
-    }
 }
